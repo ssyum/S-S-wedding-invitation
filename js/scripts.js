07faaa6a7604315b4cc28330f4b213e85bda42c5
ssyum <sung.soo@icloud.com>
2025-01-15 19:36:39 +0100

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'd6ab9c3bee303814b49e3fa3ff6bc4e4') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbxcm5ZpgUwxZwgwKImtvSfN_IoF4WVjJtSZNmltmOWs7Pd0livsFfEPG-t5VizpHg/exec', data)
