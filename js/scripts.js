0a92ad73057f8ea2bf7c090420caeae6f891b095
ssyum <sung.soo@icloud.com>
2025-01-17 16:13:33 +0100

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -156,23 +156,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Su Jung and Sungsoo's wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jun 07, 2025 14:30'),
 
             // Event duration (IN MINUTES)
-            // duration: 120,
+            // duration: 150,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jun 07, 2025 17:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Friedrichsruher Str. 6a, 12169 Berlin',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any questions, please contact us at +49 15151918362."
         }
     });
 
